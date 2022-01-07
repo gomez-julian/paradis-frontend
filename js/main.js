0d1a7c3643de00e5de22c6051adf6111ef3fe98f
gomez-julian <18161126@itoaxaca.edu.mx>
2022-01-07 14:00:55 -0600

    Establecimiento del repositorio

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+        $(document).ready(function () {
+          $(".menu-container").load("nav.html");
+		  $(".footer-container").load("footer.html");
+        });
+      
+
+(function($) {
+
+	"use strict";
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+})(jQuery);
